4211dff8fb1321c8babadf42e68ed7452d352ed2
Pedro Gentil <pgentil@udesa.edu.ar>
2022-03-09 23:39:53 -0300

    Primer commit

---
+++ tutorial_08-03.py
@@ -0,0 +1,5 @@
+
+VAR1 = 10
+var2 = "Hello World"
+var3 = None
+var4 = 3.5
---
+++ tutorial_08_03.py
@@ -0,0 +1,76 @@
+#%% VARIABLES
+'Variables'
+# var1 = 10
+# var2 = "Hello World"
+# var3 = None
+# var4 = 3.5
+
+# if 0:
+#     print ("hello world 0") #el 0 fnciona como Falsey
+    
+# if 1:
+#     print ("hello world 1") #el 1 funciona como Truthy
+    
+# x1 = 100
+# x2 = 20
+# x3 = -5
+# y = x1 + x2 + x3
+# z = x1 - x2 * x3
+# w = (x1+x2+x3) - (x1-x2*x3)
+
+# num = 23
+# data = True
+# var = 40.0
+# res1 = num + data 
+# res2 = data/var
+# res3 = num*var
+
+# num = 23
+# data = False
+# var = 40.0
+# res1 = num + data 
+# res2 = data/var
+# res3 = num*var
+
+# result = 70
+# data = False
+# value = '158'
+# var1 = result*data
+# var2 = data+value
+# var3 = result/value
+
+# result = 5
+# value = '158'
+# location = 'payunia'
+
+# name = 'Mike '
+# phrase = 'needs a coffee'
+# full_phrase = name + phrase
+# extended_phrase = full_phrase + ' urgente!'
+
+# subnet = '192.168.0'
+# host = '34'
+# ip = subnet + '.' + host
+# message = 'IP address: ' + ip
+
+#%% ACTIVIDADES
+'Actividad 1'
+a = 50
+b = 6
+c = 8
+d = 2*a + 1/(b-5*c)
+d1 = ((a*b+c)/(2-a) + ((a*b+c)/(2-a) + 2)/(c+b)) * (1 + (a*b+c)/(2-a))
+'Actividad 2'
+# word0 = 'Life'
+# word1 = 'ocean'
+# word2 = 'up'
+# word3 = 'down'
+# word4 = word0 + ' is like the ' + word1 + ", it goes " + word2 +\
+#  " and " + word3
+
+word0 ='Mars'
+word1 = 'Earth'
+word2 = 'round'
+word3 = 'round'
+word4 = word0 + ' is like the ' + word1 + ", it goes " + word2 +\
+ " and " + word3
