14c50ec680e210dcf15144ad15a40932277274b9
pgentil <pgentil@udesa.edu.ar>
2022-05-16 07:58:32 -0300

    guia de archivos y TP3

--- practica/guia6.py
+++ practica/guia6.py
@@ -3,7 +3,7 @@ def less():
     with open('texto.txt', 'r') as f:
         for line in f:
             print(line)
-
+    return None
 
 #%% Ej 2
 
@@ -11,6 +11,7 @@ def head(filename, N):
     with open(filename, 'r') as f:
         for lines in range(0, N):
             print(f.readlines(), end= '')
+    return None
 
 #%% EJ 3
 
@@ -19,12 +20,14 @@ def tails(filename, num_lines):
         lines = f.readlines()
         for line in lines[len(lines)- num_lines:]:
             print(line, end="")
+    return None
 
 #%% EJ 4
 
 def touch(filename):
     with open(filename, 'a'):
         pass 
+    return None
 
 #%% EJ 5
 def cp(filename, newfilename):
@@ -32,31 +35,92 @@ def cp(filename, newfilename):
         for lines in f.readlines():
             h.write(lines) 
         pass
-    pass
+    return None
 
 
 #%% EJ 6
 def wc(filename):
-    lineas = 0
     palabras = 0
     caracteres = 0
     with open(filename, 'r') as f:
-        print(f.readlines())
-        for lines in f.readlines():
-            lineas =+ 1
-            lista = lines.split(" ")
-            palabras =+ len(lista)
-            for palabra in lista:
-                caracteres =+ len(palabra)
-        return lineas, palabras, caracteres, filename
+        lineas = f.readlines()
+        n_lineas = len(lineas)
+        for line in lineas:
+            invalid = line.count(" ") + line.count("\n")
+            print (len(line))
+            o = line.split(" ")
+            palabras = palabras + len(o)
+            caracteres = caracteres + len(line) - invalid
+            print("o:", o)
+        return n_lineas, palabras, caracteres, filename
+
+
+#%% Ej 7
+def grep(string: str, filename:str):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if string in line:
+                print(line.strip("\n"))
+        return None
+
+#%%Ej 8
+def cat(filename1:str, filename2:str, newfile: str = "newfile.txt"):
+    with open (filename1, 'r') as f, open (filename2, 'r') as g, open (newfile, 'w') as h:
+        h.write("First file\n")
+        for line in f.readlines():
+            h.write(line)
+        h.write("\nSecond file\n")
+        for line2 in g.readlines():
+            h.write(line2)
+        pass
+    return None
+
+
+#%%Ej 9
+
+#%%Ej 10
+def load_data(filename: str) -> dict:
+    dictionary = {}
+    with open (filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip("\n")
+            if len(line.split(":")) == 2:
+                key, value = line.split(":")
+                dictionary[key] = value
+        return dictionary
+
+#%% Ej 11
+def save_data(dictionary: dict, filename: str):
+    with open(filename, 'a') as f:
+        for key in dictionary.keys():
+            f.write(f'{key}:{dictionary[key]}\n')
+        return None
+
+#%% Ej 12
+
+
 
 def main():
+    # dictionary = {
+    #     'Pasto' : 'verde',
+    #     'Cielo' : 'celeste',
+    #     'Nube' : 'blanca'
+    # }
+
     #less()
-    head('texto.txt', 3)
-    tails('texto.txt', 3)
-    touch('helo.txt')
-    cp('texto.txt', "copy.txt")
-    print(wc("texto.txt"))
+    #head('texto.txt', 3)
+    #tails('texto.txt', 3)
+    #touch('helo.txt')
+    #cp('texto.txt', "copy.txt")
+    #print(wc("texto.txt"))
+    #grep('a', 'texto.txt')
+    #cat("copy.txt", "texto.txt")
+    #load_data("helo.txt")
+    #save_data(dictionary, "save_data.txt")
+
+    pass
 
 if __name__ == "__main__":
     main()
---
+++ tutoriales/clase10-05/TP3.py
@@ -0,0 +1,10 @@
+#clases
+#__init__ --> constructor
+#self --> para identificar los metodos de la clase
+#la clase es como un plano y el __init__ te lo construye
+# metodos son las funciones dentro de la clase, y deben tener siempre el self
+#getters y setters (metodos) --> para ver un atributo del  objeto en cuestion, porque no es buena practica acceder a esos atributos directamente
+#los setters son para setear un nuevo valor de algun atrubuto
+#los getters son para acceder a atributos del objeto
+#reificar
+
